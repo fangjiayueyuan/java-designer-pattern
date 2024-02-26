c2fa677bb8aafb4cae93817a27d9a4e633f5d2f6
fangjiayueyuan <fangjiayueyuan@163.com>
2024-02-26 23:38:43 +0800

    建造者模式

--- src/main/java/builderPattern/Main.java
+++ src/main/java/builderPattern/Main.java
@@ -15,12 +15,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        BikeDirector bikeDirector = new BikeDirector();
 
         for (int i = 0; i < n; i++) {
             String type = scanner.next();
-
+            if(type.equals("mountain")){
+                System.out.println(bikeDirector.construct(new MountainBike()));
+            }else{
+                System.out.println(bikeDirector.construct(new MountainBike()));
+            }
         }
-
-
     }
 }
